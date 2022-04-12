1e4c08cfca7b7931db2bc72e8d8189a4893f0706
sowjavempalli <97742059+sowjavempalli@users.noreply.github.com>
2022-04-12 15:54:29 +0530

    Add files via upload

---
+++ workspace1/Examples/src/training/AcessModifiers.java
@@ -0,0 +1,14 @@
+package training;
+
+public class AcessModifiers {
+	class Main {
+		  public static void main(String[] args) {
+		    String str1 = "23";
+		    String str2 = "4566";
+		    int num1 = Integer.parseInt(str1);
+		    int num2 = Integer.parseInt(str2);
+		    System.out.println(num1);   
+		    System.out.println(num2);    
+		  }
+		}
+}
